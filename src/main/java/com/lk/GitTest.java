58069ef961491f934383f56fd7fa2bad57f71c41
lkun <827350247@qq.com>
2021-11-21 15:55:01 +0800

    second commit

--- src/main/java/com/lk/GitTest.java
+++ src/main/java/com/lk/GitTest.java
@@ -4,5 +4,9 @@ package com.lk;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("ssh  push!");
     }
 }
